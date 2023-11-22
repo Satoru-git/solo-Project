0e493b7dcd43fb37b6eedd131be488259d488aec
Satoru <Satoru_OfficePC@SatoruOfficeMacBook-Pro.local>
2023-11-22 18:32:28 +0900

    fix:Client accessApi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   const [dbData, setDbData] = useState<inputDbData[]>([]);
   const [userInfo, setUserInfo] = useState<inputDbData[]>([]);
-  console.log(userInfo);
+
   useEffect(() => {
     DataApi.getAll().then((res: AxiosResponse<inputDbData[]>) => {
       setDbData(res.data);
--- client/src/routes/DataApi.tsx
+++ client/src/routes/DataApi.tsx
@@ -1,13 +1,22 @@
-import CreateRoot from './CreateRoot';
+import axios from 'axios';
+
 interface Data {
   post_id: number;
   post_text: string;
   post_imgPath: string;
   post_date: Date;
 }
+// 本番環境
 const DataApi = {
-  getAll: () => CreateRoot.get('data'),
-  createPost: (data: Data) => CreateRoot.post('data', data),
+  getAll: () => axios.get('data'),
+  createPost: (data: Data) => axios.post('data', data),
 };
+
+// 開発環境
+// import CreateRoot from './CreateRoot';
+// const DataApi = {
+//   getAll: () => axios.get('data'),
+//   createPost: (data: Data) => CreateRoot.post('data', data),
+// };
 
 export default DataApi;
