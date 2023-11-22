b78c1d71fe205796cab3564589b409bef11138d8
Satoru <Satoru_OfficePC@SatoruOfficeMacBook-Pro.local>
2023-11-22 13:31:08 +0900

    add:server index.ts PORT

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
 const app = (0, server_1.default)();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log('Root server:', `http://localhost:${PORT}`);
 });
